35aa4b43d14ff99d5adfe87c8abe11c5520c89f7
oxd <oxd@oxd.me>
2023-12-19 20:45:22 +0700

    fail with autocomplete

--- src/pages/login_form/login_form.ts
+++ src/pages/login_form/login_form.ts
@@ -5,35 +5,39 @@ import {Page} from "../Page";
 import {FIELD_NAMES} from "../../constants";
 
 export class LoginForm extends Page {
-    _registerButton?: HTMLElement;
+    private registerButton?: HTMLElement;
 
     constructor() {
         const options = {
             login: {
                 name: FIELD_NAMES.login,
+                id: FIELD_NAMES.login,
                 type: 'text',
                 value: '',
                 placeholder: 'Логин',
             },
             password: {
                 name: FIELD_NAMES.password,
+                id: FIELD_NAMES.login,
                 type: 'password',
                 value: '',
                 placeholder: 'Пароль',
             },
             login_button: {
                 name: FIELD_NAMES.login_button,
+                id: FIELD_NAMES.login,
                 className: 'mt-1 button-active',
                 text: 'Войти',
             },
             registration_button: {
                 name: FIELD_NAMES.registration_button,
+                id: FIELD_NAMES.login,
                 className: 'button-text',
                 text: 'Зарегистироваться',
             },
         }
         super(LoginFormTemplate, options)
-        this._registerButton = searchElementByName(this.div, 'registration_button');
-        this._registerButton?.addEventListener('click', () => router.navigate('/registration'))
+        this.registerButton = searchElementByName(this.div, FIELD_NAMES.registration_button);
+        this.registerButton?.addEventListener('click', () => router.navigate('/registration'))
     }
 }
